93675a1de21e0967ec3d22cbbdb1f2db18d48ba2
emotionalhandle <emotionalhandle@gmail.com>
2025-01-23 16:05:28 +0000

    Implemented a jump function
    Also added a constant downward force to make the jump feel less floaty.

--- BrackeysProjectOne/Assets/Scripts/PlayMovement.cs
+++ BrackeysProjectOne/Assets/Scripts/PlayMovement.cs
@@ -2,14 +2,37 @@ using UnityEngine;
 
 public class PlayMovement : MonoBehaviour
 {
+    private bool isGrounded;
     public Rigidbody rb;
 
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    public float jumpForce = 20f;
+    public float downwardForce = 50f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground") 
+        {
+            isGrounded = true;
+            Debug.Log("Touched ground");
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+            Debug.Log("Exit ground");
+        }
+    }
 
     void FixedUpdate()
     {
         rb.AddForce(0,0,forwardForce * Time.deltaTime);
+        rb.AddForce(0, -downwardForce * rb.mass, 0); // Add additional downward force to simulate increased gravity
+    
 
         if (Input.GetKey("d"))
         {
@@ -19,6 +42,11 @@ public class PlayMovement : MonoBehaviour
         if (Input.GetKey("a"))
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0, ForceMode.VelocityChange);
         }
 
         if (rb.position.y < -1f)
